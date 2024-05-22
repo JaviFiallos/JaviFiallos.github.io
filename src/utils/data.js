43a3a855b3bb8a4832c37e85642691bb40a869a9
JaviF <fiallosedisonreggae@gmail.com>
2024-05-22 15:16:47 -0500

    Copia del proyecto

---
+++ src/utils/data.js
@@ -0,0 +1,38 @@
+export const SKILLS = [
+    {
+        title: 'Frontend',
+        icon: './assets/images/frontendIcon.png',
+        skills: [
+            { skill: "HTML5", percentage: "80%" },
+            { skill: "CSS", percentage: "60%" },
+            { skill: "JavaScript", percentage: "75%" },
+        ],
+    },
+    {
+        title: 'Backend',
+        icon: './assets/images/backendIcon.png',
+        skills: [
+            { skill: "Java", percentage: "60%" },
+            { skill: "PHP", percentage: "40%" },
+        ],
+    },
+    {
+        title: 'Tools',
+        icon: './assets/images/toolsIcon.png',
+        skills: [
+            { skill: "Git", percentage: "50%" },
+            { skill: "Github & GitLab", percentage: "54%" },
+            { skill: "Visual Studio Code", percentage: "60%" },
+            { skill: "Netbeans", percentage: "60%" },
+        ],
+    },
+    {
+        title: 'Cursos',
+        icon: './assets/images/cursosIcon.png',
+        skills: [
+            { skill: "HTML5", percentage: "80%" },
+            { skill: "CSS", percentage: "65%" },
+            { skill: "JavaScript", percentage: "30%" },
+        ],
+    },
+];
